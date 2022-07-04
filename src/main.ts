977c2f913815aea6036ac20d439777100a1c95d9
junho100 <bemodesty306@naver.com>
2022-07-04 18:16:36 +0900

    Fix: port 변경

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.use(morgan('tiny'));
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
