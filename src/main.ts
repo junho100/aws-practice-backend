8bbbe02b5784e37ec6edaff3042321e02db4eaa6
junho100 <bemodesty306@naver.com>
2022-07-15 20:58:06 +0900

    ...

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'crn-junho-static-web.s3-website.ap-northeast-2.amazonaws.com',
+    origin:
+      'http://crn-junho-static-web.s3-website.ap-northeast-2.amazonaws.com',
     credentials: true,
   });
   app.use(morgan('tiny'));
